218e1cb1017f51e040fd2417697cb53b8fc0e81b
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-05-28 16:51:15 +0300

    Updates

--- static/js/ErrorPage.4beda345.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksuperhero_front=self.webpackChunksuperhero_front||[]).push([[98],{820:function(r,e,n){n.r(e),n.d(e,{default:function(){return t}});var u=n(184);function t(){return(0,u.jsx)("div",{children:"Error page"})}}}]);
-//# sourceMappingURL=ErrorPage.4beda345.chunk.js.map
